ba882662239b5aebbc03c95366b850457588fb54
djangodissilent <ahmed.amein.rslan@gmail.com>
2022-03-17 12:52:41 +0200

    modified config.py

--- config.py
+++ config.py
@@ -1,14 +1,3 @@
-# DEFUALT
-#
-# confing = {
-#     'data_type': 4,  # 1: Live, 2: frozen, 3: delayed, 4: delayed frozen | set 1 at production
-#     'polling_interval': 1,  # in seconds
-#     'server_port': 5000,
-#     'ib_host': '127.0.0.1',
-#     'ib_port': 7497,
-# }
-
-
 
 """
     Configuration for the server and the bot
@@ -20,3 +9,17 @@ config = {
     'ib_host': '127.0.0.1',
     'ib_port': 7497,
 }
+
+
+# DEFUALT
+# --------------------------------------------------
+# --------------------------------------------------
+# confing = {
+#     'data_type': 4,  # 1: Live, 2: frozen, 3: delayed, 4: delayed frozen | set 1 at production
+#     'polling_interval': 1,  # in seconds
+#     'server_port': 5000,
+#     'ib_host': '127.0.0.1',
+#     'ib_port': 7497,
+# }
+
+
